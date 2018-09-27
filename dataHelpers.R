a078528e10fcca69dcaee4b12766d30c09a56adc
Casey Woolfolk <ccwoolfolk@gmail.com>
2018-09-27 09:00:47 -0500

    Convert data storage/retrieval to use demandmap

--- dataHelpers.R
+++ dataHelpers.R
@@ -1,9 +1,25 @@
+
+
 getData <- function (name) {
-  tmpEnv <- new.env()
-  load(file=paste0('data/', name, '.RData'), envir=tmpEnv)
-  return(get(name, envir=tmpEnv))
+  mfg <- demandmap::getData()
+  
+  ### Create a list of unique NAICS codes
+  allNaicsCodes <- mfg %>% pull(NAICS.id)
+  naicsCodes <- unique(allNaicsCodes)
+  naicsCodes <- naicsCodes[order(naicsCodes)]
+  if (name == 'naicsCodes') return(naicsCodes)
+  
+  uniqueRows <- match(naicsCodes, allNaicsCodes)
+  naicsNames <- (mfg %>% pull(`NAICS.display-label`))[uniqueRows]
+  if (name == 'naicsNames') return(naicsNames)
+  
+  naicsLabels <- paste(str_pad(naicsCodes, width=max(nchar(naicsCodes)), side='right', '_'), naicsNames, sep=' : ')
+  if (name == 'naicsLabels') return(naicsLabels)
+
+  stop('"name" not recognized')
 }
 
+### TODO: is this duplicative with getData('naicsLabels') ?
 getNaicsLabels <- function(codes) {
   naicsCodes <- getData('naicsCodes')
   naicsNames <- getData('naicsNames')
